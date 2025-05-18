e8150e77e30b2e924943d23a18b33473fdc8e021
Runju-Raton <68098346+Runju-Raton@users.noreply.github.com>
2025-05-18 13:09:11 +0600

    example 12

---
+++ example12.py
@@ -0,0 +1,7 @@
+# list print using range from 1 to 10
+my_list = list(range(1,11))
+print('List ',my_list)
+
+# even number list print using range from 1 to 20
+li = list(range(2,21,2))
+print('Even Number List',li)
